78b527e4d6f1d6eb8bdedbb6c0a07240ec3bd593
legend76565 <62374576+legend76565@users.noreply.github.com>
2021-05-18 12:35:46 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -61,7 +61,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by ΛLPHA ᴍᴜꜱɪᴄ **'
+        rep = '**🎵 Uploaded by Camila ᴍᴜꜱɪᴄ **'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
