b00d453836d3da0614c7505dcf0a38c79b09e416
legend76565 <62374576+legend76565@users.noreply.github.com>
2021-05-19 20:35:33 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -14,7 +14,7 @@ async def start_(client: Client, message: Message):
         f"""<b>Hi {message.from_user.first_name}!
 \nI can play music in your group's voice chat
 Maintained by @itsMeLegend ❤
-
+Music Assistant - @camilaowner
 \nTo add in your group contact us at @camila_Support
 \nHit /help list of available commands.
  </b>""",
